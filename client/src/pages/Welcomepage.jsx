251f542173fd4248f050b92f90d456f18ac58d88
kanha_1 <kanhu.sahoo09@gmail.com>
2021-02-18 14:27:47 +0530

    added

--- client/src/pages/Welcomepage.jsx
+++ client/src/pages/Welcomepage.jsx
@@ -4,7 +4,7 @@ export class Welcomepage extends Component {
   render() {
     return (
       <div>
-        <h2 style={{ margin: "150px 50px" ,color:"lightblue"}}>
+        <h2 style={{ margin: "150px 50px" ,color:"Blue"}}>
            please login to order products!
         </h2>
       </div>
